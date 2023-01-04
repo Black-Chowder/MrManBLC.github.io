2e1271ba6f61f4d39bfe238add134d5413a9c2e6
MrManBLC <gman.dude132@gmail.com>
2023-01-03 19:21:24 -0500

    Separation between nav bar and page contents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-extrabold">
-        Hello World!
-      </h1>
+    <div className="main box">
+      <div className="row content">
+
+        <div className="flex flex-row h-full bg-gradient-to-br from-space-cadet-400 to-space-cadet-700">
+
+          {/* left-handed nav bar */}
+          <div className="flex w-72 grow-0 bg-space-cadet-700">
+
+
+
+          </div>
+
+          {/* main content */}
+          <div className="flex w-full grow bg-opacity-0">
+
+            
+
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
+    colors: {
+      'platinum': '#EAEAEA',
+      'lavender': '#CBC5EA',
+      'lilac': '#73628A',
+      'space-cadet-400': '#6176A8',
+      'space-cadet-500': '#506391',
+      'space-cadet-600': '#415176',
+      'space-cadet-700': '#313D5A',
+      'space-cadet-800': '#242D42',
+      'space-cadet-900': '#161B27',
+      'gunmetal': '#183642'
+    }
   },
   plugins: [],
 }
