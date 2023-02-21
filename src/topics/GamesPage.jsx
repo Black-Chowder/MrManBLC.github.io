edff3b4983f8beb7f1af8f37cb531c73f044a1a9
MrManBLC <gman.dude132@gmail.com>
2023-02-21 14:39:18 -0500

    Popup next to game title

--- src/topics/GamesPage.jsx
+++ src/topics/GamesPage.jsx
@@ -4,6 +4,7 @@ import title_Frogs from '../imgs/games/title_Frogs.png';
 import title_DevilInHeaven from '../imgs/games/title_DevilInHeaven.png';
 import title_LittleSheepOfMine from '../imgs/games/title_LittleSheepOfMine.png';
 import '../NavBar.css';
+import { Children, useState } from 'react';
 
 export const GamesPage = () => {
 
@@ -13,7 +14,13 @@ export const GamesPage = () => {
       <div className="flex flex-wrap gap-10">
 
         <Game title="Little Sheep of Mine"
-          thumbnailSrc={title_LittleSheepOfMine}/>
+          thumbnailSrc={title_LittleSheepOfMine}
+          description="This is a test description">
+
+            <p className="text-lg font-semibold">A scared lamb runs away from the cruel care of wicked Shepard Jack, only to run into yet another sticky situation - Hell itself!</p>
+            <br/>
+            <p className="text-lg font-semibold">Help her fight off the incoming hordes of demons!</p>
+        </Game>
         
         <Game title="Dicey Defense" 
           thumbnailSrc={title_DiceyDefense}/>
@@ -35,25 +42,42 @@ export const GamesPage = () => {
 
 //Note: Src img ideal ratio = 630x500
 //      Minimum = 315x250
-const Game = ({ title, thumbnailSrc, description }) => {
+const Game = ({ title, thumbnailSrc, description, children }) => {
+  const [popupActive, setPopupActive] = useState(false);
+
   const MouseOver = (e) => {
-    console.log("Mouse entered");
+    setPopupActive(true);
   }
 
   const MouseOut = (e) => {
-    console.log("Mouse left");
+    setPopupActive(false);
   }
 
-
+  const Popup = ({children}) => {
+    return(
+      <div className={`absolute w-full h-full -right-full z-10`}
+        style={{opacity: `${popupActive ? 1 : 0}`, transition: 'all 0.3s', pointerEvents: `${popupActive ? 'auto' : 'none'}`}}>
+        
+        <div className="ml-3 w-full h-full bg-gradient-to-br from-lilac to-space-cadet-400 rounded-xl p-3 overflow-hidden">
+          
+          {children}
+        </div>
+      </div>
+    )
+  }
 
   return(
     <div className="w-72 hover:w-96 h-fit bg-gradient-to-br from-lilac to-space-cadet-400 rounded-xl relative"
-      style={{transition: "all 0.3s"}}>
+      style={{transition: "all 0.3s", width: `${popupActive ? '24' : '18'}rem`}}>
 
       {/* Overlay */}
       <div className="w-full h-full absolute"
         onMouseOver={MouseOver}
         onMouseLeave={MouseOut}/>
+
+      <Popup>
+        {children}
+      </Popup>
 
       {/* Display Content */}
       <div className="h-16 flex justify-center pt-4">
